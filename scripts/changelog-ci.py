397352519e89b79e2a41886fe7ed5bbb87ed9767
Kalp <kalp.shah97@gmail.com>
2021-03-18 22:11:23 -0400

    Added brackets

--- scripts/changelog-ci.py
+++ scripts/changelog-ci.py
@@ -384,7 +384,7 @@ class ChangelogCI:
             )
             _print_output('error', msg)
             return
-        version += f" {datetime.now().strftime('%m/%d/%Y')}"
+        version += f" ({datetime.now().strftime('%m/%d/%Y')})"
 
         tags = self._validate_tags()
         if not tags:
